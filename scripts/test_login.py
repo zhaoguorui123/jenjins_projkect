71570a613230221ae10d20320aae4e1158d74151
zhaoguorui123 <1610301229@qq.com>
2021-02-04 17:37:00 +0800

    www

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_hello1(self):
+        assert True
+
+    def test_hello2(self):
+        assert True
+
+    def test_hello3(self):
+        assert True
+
+    def test_hello4(self):
+        assert True
